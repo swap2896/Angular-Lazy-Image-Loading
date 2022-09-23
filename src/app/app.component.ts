c939e50ab85ffa4e322944dea3581edfa6ce14b8
swap2896 <kalra.swapnill2896@gmail.com>
2022-09-23 14:44:27 +0530

    added Lazy Loading image code for angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  screenWidth = window.innerWidth;  
+  screenHeight = window.innerHeight;  
   title = 'lazy-load-images-angular';
+  image1 = 'https://images.unsplash.com/photo-1581789164394-810293cd79ce';
+  image2 = 'https://images.unsplash.com/photo-1562690868-60bbe7293e94';
+  image3 = 'https://images.unsplash.com/photo-1536677813196-8fed27bcecdc';
+  image4 = 'https://images.unsplash.com/photo-1599198688091-926a8df3c9be';
 }
